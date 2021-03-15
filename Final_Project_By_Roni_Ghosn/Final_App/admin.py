97529ff66aaaa8475584415f9ee4c5e083130b5e
Roni <ronitaboosh@gmail.com>
2021-03-15 14:47:34 +0200

    RoniGhosnFinalproject

---
+++ Final_Project_By_Roni_Ghosn/Final_App/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from Final_App.models import UserProfileInfo, Vacation
+
+# Register your models here.
+admin.site.register(UserProfileInfo)
+admin.site.register(Vacation)
---
+++ Final_Project_By_Roni_Ghosn/Final_App/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from Final_App.models import User, UserProfileInfo, Vacation
+from django.contrib.auth.models import User
+from tkinter.test.support import widget_eq
+from Final_App import models
+from dataclasses import fields
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password')
+
+
+class UserProfileInfoForm(forms.ModelForm):
+    class Meta():
+        model = UserProfileInfo
+        fields = ('job_position', 'profile_pic', 'Date_Of_Birth')
+        widgets = {
+            'Date_Of_Birth': forms.DateInput(attrs={'class': 'datepicker'}),
+        }
+
+
+class vacation(forms.ModelForm):
+    class Meta():
+        model = Vacation
+        fields = ('description', 'date_from', 'date_to')
