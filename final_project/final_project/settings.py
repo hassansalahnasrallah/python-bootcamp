ef58c80b5f0c87e3e1be2c3d8d32e2a24f9c6009
daliaabk <daliaaboukarroum@gmail.com>
2021-04-03 13:20:57 +0300

    New version

--- final_project/final_project/settings.py
+++ final_project/final_project/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_DIR = os.path.join(BASE_DIR,'media')
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
@@ -136,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    STATIC_DIR,
+    #STATIC_DIR,
     ]
 
 MEDIA_ROOT = MEDIA_DIR
